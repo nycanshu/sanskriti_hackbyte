f93af2d67410b38dbdc740e084deeda103b387a8
Yuvaraj Dekhane <148527838+yuvaraj8080@users.noreply.github.com>
2024-04-07 02:32:26 +0530

    hi

---
+++ lib/yuvaraj_screen/feed_screen.dart
@@ -0,0 +1,38 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'widget2/post_card.dart';
+
+class FeedScreen extends StatefulWidget {
+  const FeedScreen({Key? key}) : super(key: key);
+
+  @override
+  State<FeedScreen> createState() => _FeedScreenState();
+}
+
+class _FeedScreenState extends State<FeedScreen> {
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('posts').snapshots(),
+        builder: (context,
+            AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return ListView.builder(
+            itemCount: snapshot.data!.docs.length,
+            itemBuilder: (ctx, index) => Container(
+              child: PostCard(
+                snap: snapshot.data!.docs[index].data(),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
---
+++ lib/yuvaraj_screen/user_provider.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/widgets.dart';
+import 'package:sanskriti/yuvaraj_screen/resources/auth_methods.dart';
+
+import '../model2/user.dart ';
+
+class UserProvider with ChangeNotifier {
+  User? _user;
+  final AuthMethods _authMethods = AuthMethods();
+
+  User get getUser => _user!;
+
+  Future<void> refreshUser() async {
+    User user = await _authMethods.getUserDetails();
+    _user = user;
+    notifyListeners();
+  }
+}
